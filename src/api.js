8a22e5679b9071fdae98802dd12e16dcc611b2c8
mernprateek <132909313+mernprateek@users.noreply.github.com>
2023-08-04 11:02:13 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 // Function to fetch all items
 export const fetchItems = async () => {
   try {
-    const response = await axios.get('http://localhost:5000/api/items');
+    const response = await axios.get('https://vast-windbreaker-duck.cyclic.app/api/items');
     return response.data;
   } catch (error) {
     console.error('Failed to fetch items:', error);
